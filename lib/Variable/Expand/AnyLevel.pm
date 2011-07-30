d19f6dd1c24d21834e3b56cf20f7b6dd38554e06
tsucchi <takuya.tsuchida at gmail.com>
2011-07-30 16:17:41 +0900

    initial commit

---
+++ lib/Variable/Expand/AnyLevel.pm
@@ -0,0 +1,32 @@
+package Variable::Expand::AnyLevel;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Variable::Expand::AnyLevel -
+
+=head1 SYNOPSIS
+
+  use Variable::Expand::AnyLevel;
+
+=head1 DESCRIPTION
+
+Variable::Expand::AnyLevel is
+
+=head1 AUTHOR
+
+Takuya Tsuchida E<lt>tsucchi@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
