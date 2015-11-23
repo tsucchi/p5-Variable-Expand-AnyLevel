00b58b6a5dbb5e6860c6d4ab9af8f1e66794f717
Takuya Tsuchida <tsucchi@cpan.org>
2015-11-23 11:40:40 +0900

    version++(updated by minilla)

--- lib/Variable/Expand/AnyLevel.pm
+++ lib/Variable/Expand/AnyLevel.pm
@@ -2,7 +2,7 @@ package Variable::Expand::AnyLevel;
 use parent qw(Exporter);
 use strict;
 use warnings;
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 our @EXPORT_OK = qw(expand_variable);
 use PadWalker qw(peek_my);
 
